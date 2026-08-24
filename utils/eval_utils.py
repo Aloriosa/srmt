84a25aef25eb82cbc64c928885e8812841b0a12a
Alsu Sagirova <34598622+Aloriosa@users.noreply.github.com>
2026-08-24 18:43:58 +0300

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -15,23 +15,33 @@ def run_episode(env, algo, log_path=None):
     """
     algo.reset_states()
     results_holder = ResultsHolder()
-    obs, reset_infos = env.reset(seed=env.grid_config.seed)
+    obs, reset_infos = env.reset(seed=env.grid_config.seed if getattr(env, 'grid_config', False) else 0)
     episode_log = [{'obs': obs, 'infos': reset_infos, 'step': 0}]
     infos = None
     while True:
         actions, policy_outputs = algo.act(obs, infos=infos)
         obs, rew, dones, tr, infos = env.step(actions)
+        #if getattr(env, 'print_elapsed_steps', False):
+        #    print(f"evl utls rew {rew}, step # {env.print_elapsed_steps}, dones {sum(dones)}, tr {sum(tr) }")
+        #else:
+        #    print(f"evl utls rew {rew}, dones {sum(dones)}, tr {sum(tr) }")
+        
         step_outputs = policy_outputs.copy()
-        step_outputs['step'] = env.print_elapsed_steps
+        step_outputs['step'] = env.print_elapsed_steps if getattr(env, 'print_elapsed_steps', False) else 0
         step_outputs['obs'] = obs
         step_outputs['rew'] = rew
         step_outputs['dones'] = dones
         step_outputs['tr'] = tr
         step_outputs['infos'] = infos
         episode_log.append(step_outputs)
+        #print(len(episode_log))
+        #print(f"upd obs = {obs}")
         results_holder.after_step(infos)
         if log_path is not None:
             np.save(log_path, episode_log, allow_pickle=True)
+            #with open(log_path + '/episode_log.json', 'w') as f:
+            #    json.dump(episode_log, f)
+
         if all(dones) or all(tr):
             break
     
